18448461348462fd533077ab52b3b0757816e5d3
ZulfiFazhar <zulfi.fazhar12@gmail.com>
2025-12-23 16:55:40 +0700

    Refactor CI/CD workflow and user controller formatting

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,31 +1,34 @@
-import { createUserService, deleteUserByIdService, getListUsersService } from "../services/userService.js";
+import {
+  createUserService,
+  deleteUserByIdService,
+  getListUsersService,
+} from "../services/userService.js";
 
 const createUserController = (req, res, next) => {
-    try {
-        const request = req.body;
-        const result = createUserService(request);
+  try {
+    const request = req.body;
+    const result = createUserService(request);
 
-        res.status(201).json({
-            message: "Create User Success",
-            data: result
-        })
-
-    } catch (error) {
-        next(error)
-    }
+    res.status(201).json({
+      message: "Create User Success",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getListUsersController = (req, res, next) => {
-    try {
-        const result = getListUsersService();
+  try {
+    const result = getListUsersService();
 
-        res.json({
-            message: "Success Get All Users",
-            data: result
-        })
-    } catch (error) {
-        next(error)
-    }
+    res.json({
+      message: "Success Get All Users",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 // const deleteUserByIdController = (req, res, next) => {
@@ -42,7 +45,7 @@ const getListUsersController = (req, res, next) => {
 // };
 
 export {
-    createUserController,
-    getListUsersController,
-    deleteUserByIdController
-}
+  createUserController,
+  getListUsersController,
+  // deleteUserByIdController
+};
